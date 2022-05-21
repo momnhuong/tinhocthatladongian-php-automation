52c3be03ce571ab3e0f27078bddaf6ed197807f7
momnhuong <songbao2903@gmail.com>
2022-05-21 11:00:26 +0700

    add B

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "AAAA";
+        $staff->first_name = "BBBB";
         $this->assertEquals('AAAA', $staff->getFullName());
     }
 }
