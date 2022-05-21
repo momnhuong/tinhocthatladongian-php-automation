8ae05e8c461a09aaa06d033f9bf01660f01b2d8e
momnhuong <songbao2903@gmail.com>
2022-05-21 10:24:21 +0700

    add FFF

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "EEE";
+        $staff->first_name = "FFF";
         $this->assertEquals('AAA', $staff->getFullName());
     }
 }
